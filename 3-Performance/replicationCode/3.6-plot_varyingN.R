4d8d17dbd957b771b68ca3438f1e2f82bd303f9e
theo-s <theo_s@sonic.net>
2021-06-22 16:27:32 -0700

    Update the plotting code

--- 3-Performance/replicationCode/3.6-plot_varyingN.R
+++ 3-Performance/replicationCode/3.6-plot_varyingN.R
@@ -24,7 +24,7 @@ library(reshape)
 library(viridis)
 
 X <- read.csv("replicationCode/3-run_all_cluster_resultsEMSE.csv", stringsAsFactors = FALSE)
-# X <- X[22:36,-1]
+# X <- X[1:15,-1]
 X$n <- as.numeric(gsub(pattern = "([^0123456789])", replacement = "", X$Dataset))
 X$Dataset <- gsub(pattern = "([-0123456789])", replacement = "", X$Dataset)
 X <- melt(data = X, id.vars = c("Dataset", "n"))
@@ -50,10 +50,15 @@ X %>% filter(!is.na(value)) %>%
   facet_wrap(.~Dataset, scales = "free_y") +
   #geom_text(aes(label = variable)) +
   theme_bw() +
-  ylim(0, 2.5) +
+  ylim(0, 5.5) +
   theme(legend.position = "none")
 
 library(ggrepel)
+
+colors = c("#440154FF", "#443A83FF",
+          "#31688EFF", "#21908CFF",
+          "#35B779FF", "#8FD744FF", "#FDE725FF",
+          "#6DCD59FF", "#1F9E89FF")
 
 # Plot experiment 1
 for (expnm in c("Experiment_1")) {
@@ -72,24 +77,21 @@ for (expnm in c("Experiment_1")) {
     geom_line() +
     #geom_text(aes(label = variable)) +
     theme_bw() +
-    xlim(0, 2500) +
+    xlim(0, 2800) +
     ylim(-.2, max + .1)+
     theme(legend.position = "none") +
     geom_text_repel(
       aes(label = variable),
-      data = end_values, color = c("#440154FF", "#443A83FF",
-                                   "#31688EFF", "#21908CFF",
-                                   "#35B779FF", "#8FD744FF", "#FDE725FF",
-                                   "#6DCD59FF", "#1F9E89FF"),
+      data = end_values, color = colors ,
       size = 3, force = 7,arrow = arrow(length = unit(0.01, "npc")),
-      direction = "both", nudge_x = 80, nudge_y = .1, point.padding = .9
+      direction = "both", nudge_x = 250, nudge_y = .15, point.padding = .7
     )+
     # ggtitle(label = expnm) +
     ggtitle(label = "") +
     xlab("Sample Size") +
     ylab("EMSE") +
     geom_point() +
-    scale_color_viridis_d()
+    scale_color_manual(values = colors)
 
   ggsave(file = paste0("figures/varyn_", expnm, ".pdf"), height = 3.3, width = 6)
   #ggsave(file = paste0("~/Dropbox/RidgeForestry_paper/figures/2-VaryNSim/varyn_",
@@ -115,24 +117,21 @@ for (expnm in c("Experiment_2")) {
     geom_line() +
     #geom_text(aes(label = variable)) +
     theme_bw() +
-    xlim(0, 2500) +
+    xlim(0, 2800) +
     ylim(-.1, max + .1)+
     theme(legend.position = "none") +
     geom_text_repel(
       aes(label = variable),
-      data = end_values, color = c("#440154FF", "#443A83FF",
-                                   "#31688EFF", "#21908CFF",
-                                   "#35B779FF", "#8FD744FF", "#FDE725FF",
-                                   "#6DCD59FF", "#1F9E89FF"),
+      data = end_values, color = colors,
       size = 3, force = 3,arrow = arrow(length = unit(0.01, "npc")),
-      direction = "both", nudge_x = 25, nudge_y = 0, point.padding = .8
+      direction = "both", nudge_x = 250, nudge_y = 0.18, point.padding = .8
     )+
     # ggtitle(label = expnm) +
     ggtitle(label = "") +
     xlab("Sample Size") +
     ylab("EMSE") +
     geom_point() +
-    scale_color_viridis_d()
+    scale_color_manual(values = colors)
 
   ggsave(file = paste0("figures/varyn_", expnm, ".pdf"), height = 3.3, width = 6)
   #ggsave(file = paste0("~/Dropbox/RidgeForestry_paper/figures/2-VaryNSim/varyn_",
@@ -158,24 +157,21 @@ for (expnm in c("Experiment_3")) {
     geom_line() +
     #geom_text(aes(label = variable)) +
     theme_bw() +
-    xlim(0, 2500) +
+    xlim(0, 2800) +
     ylim(-.1, max + .1)+
     theme(legend.position = "none") +
     geom_text_repel(
       aes(label = variable),
-      data = end_values, color = c("#440154FF", "#443A83FF",
-                                   "#31688EFF", "#21908CFF",
-                                   "#35B779FF", "#8FD744FF", "#FDE725FF",
-                                   "#6DCD59FF", "#1F9E89FF"),
+      data = end_values, color = colors,
       size = 3, force = 3,arrow = arrow(length = unit(0.01, "npc")),
-      direction = "both", nudge_x = 25, nudge_y = 0, point.padding = .92
+      direction = "both", nudge_x = 100, nudge_y = 0.1, point.padding = .92
     )+
     # ggtitle(label = expnm) +
     ggtitle(label = "") +
     xlab("Sample Size") +
     ylab("EMSE") +
     geom_point() +
-    scale_color_viridis_d()
+    scale_color_manual(values = colors)
 
   ggsave(file = paste0("figures/varyn_", expnm, ".pdf"), height = 3.3, width = 6)
   #ggsave(file = paste0("~/Dropbox/RidgeForestry_paper/figures/2-VaryNSim/varyn_",
